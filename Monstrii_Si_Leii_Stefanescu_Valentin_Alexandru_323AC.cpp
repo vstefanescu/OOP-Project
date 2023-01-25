8a77d8fe03f641a996325d427a227ece4a8b9628
Stefanescu Valentin Alexandru <91956540+vstefanescu@users.noreply.github.com>
2023-01-25 22:38:33 +0200

    Update Monstrii_Si_Leii_Stefanescu_Valentin_Alexandru_323AC.cpp

---
+++ Monstrii_Si_Leii_Stefanescu_Valentin_Alexandru_323AC.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+#include <fstream>
+#include <iomanip>
+#include <chrono>
+
+#define dollarExchangeRate 4.49
+#define figureNumber 132
+#define namesFromDB 1000
+#define busPrice 5680
+#define max 1000
+
+int main()
+{
+    
+}
